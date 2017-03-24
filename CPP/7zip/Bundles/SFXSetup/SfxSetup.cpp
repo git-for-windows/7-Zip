e71bb3b3f9267484701580fa0700cc1b34d5532a
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-03-24 15:53:13 +0100

    SFXSetup: support `ExtractDialogText` config setting
    This feature imitates the (now-defunct) modified SFX component from the
    (now-defunct) http://7zsfx.info/ website.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- CPP/7zip/Bundles/SFXSetup/SfxSetup.cpp
+++ CPP/7zip/Bundles/SFXSetup/SfxSetup.cpp
@@ -164,7 +164,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /* hPrevInstance */,
   }
 
   UString dirPrefix ("." STRING_PATH_SEPARATOR);
-  UString appLaunched;
+  UString appLaunched, extractDialogText;
   bool showProgress = true;
   if (!config.IsEmpty())
   {
@@ -178,6 +178,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /* hPrevInstance */,
     const UString friendlyName = GetTextConfigValue(pairs, "Title");
     const UString installPrompt = GetTextConfigValue(pairs, "BeginPrompt");
     const UString progress = GetTextConfigValue(pairs, "Progress");
+    extractDialogText = GetTextConfigValue(pairs, "ExtractDialogText");
     if (progress.IsEqualTo_Ascii_NoCase("no"))
       showProgress = false;
     const int index = FindTextConfigItem(pairs, "Directory");
@@ -222,7 +223,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /* hPrevInstance */,
     bool isCorrupt = false;
     UString errorMessage;
     HRESULT result = ExtractArchive(codecs, fullPath, tempDirPath, showProgress,
-      isCorrupt, errorMessage);
+      extractDialogText, isCorrupt, errorMessage);
     
     if (result != S_OK)
     {
